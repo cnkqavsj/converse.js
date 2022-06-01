5ae9a3aa73a16c9951c210d135d3ef9eb27231c9
JC Brand <jc@opkode.com>
2022-06-01 22:04:08 +0200

    Fixup

--- src/modals/add-contact.js
+++ src/modals/add-contact.js
@@ -1,7 +1,6 @@
 import 'shared/autocomplete/index.js';
 import BootstrapModal from "plugins/modal/base.js";
 import compact from 'lodash-es/compact';
-import debounce from 'lodash-es/debounce';
 import tpl_add_contact_modal from "./templates/add-contact.js";
 import { __ } from 'i18n';
 import { _converse, api, converse } from "@converse/headless/core";
@@ -22,91 +21,8 @@ const AddContactModal = BootstrapModal.extend({
         return tpl_add_contact_modal(this);
     },
 
-    afterRender () {
-        const jid_input = this.el.querySelector('input[name="jid"]');
-        this.el.addEventListener('shown.bs.modal', () => jid_input.focus(), false);
-    },
-
     getGroupsAutoCompleteList () {
-        return ['apple', 'pear', 'banana'];
-        // return [...new Set(_converse.roster.map(i => i.get('gruop')).filter(i => i))];
-    },
-
-    initJIDAutoComplete () {
-        if (!api.settings.get('autocomplete_add_contact')) {
-            return;
-        }
-        const el = this.el.querySelector('.suggestion-box__jid').parentElement;
-        this.jid_auto_complete = new _converse.AutoComplete(el, {
-            'data': (text, input) => `${input.slice(0, input.indexOf("@"))}@${text}`,
-            'filter': _converse.FILTER_STARTSWITH,
-            'list': [...new Set(_converse.roster.map(item => Strophe.getDomainFromJid(item.get('jid'))))]
-        });
-    },
-
-    initGroupAutoComplete () {
-        if (!api.settings.get('autocomplete_add_contact')) {
-            return;
-        }
-        const el = this.el.querySelector('.suggestion-box__jid').parentElement;
-        this.jid_auto_complete = new _converse.AutoComplete(el, {
-            'data': (text, input) => `${input.slice(0, input.indexOf("@"))}@${text}`,
-            'filter': _converse.FILTER_STARTSWITH,
-            'list': [...new Set(_converse.roster.map(item => Strophe.getDomainFromJid(item.get('jid'))))]
-        });
-    },
-
-    initXHRAutoComplete () {
-        if (!api.settings.get('autocomplete_add_contact')) {
-            return this.initXHRFetch();
-        }
-        const el = this.el.querySelector('.suggestion-box__name').parentElement;
-        this.name_auto_complete = new _converse.AutoComplete(el, {
-            'auto_evaluate': false,
-            'filter': _converse.FILTER_STARTSWITH,
-            'list': []
-        });
-        const xhr = new window.XMLHttpRequest();
-        // `open` must be called after `onload` for mock/testing purposes.
-        xhr.onload = () => {
-            if (xhr.responseText) {
-                const r = xhr.responseText;
-                this.name_auto_complete.list = JSON.parse(r).map(i => ({'label': i.fullname || i.jid, 'value': i.jid}));
-                this.name_auto_complete.auto_completing = true;
-                this.name_auto_complete.evaluate();
-            }
-        };
-        const input_el = this.el.querySelector('input[name="name"]');
-        input_el.addEventListener('input', debounce(() => {
-            xhr.open("GET", `${api.settings.get('xhr_user_search_url')}q=${encodeURIComponent(input_el.value)}`, true);
-            xhr.send()
-        } , 300));
-        this.name_auto_complete.on('suggestion-box-selectcomplete', ev => {
-            this.el.querySelector('input[name="name"]').value = ev.text.label;
-            this.el.querySelector('input[name="jid"]').value = ev.text.value;
-        });
-    },
-
-    initXHRFetch () {
-        this.xhr = new window.XMLHttpRequest();
-        this.xhr.onload = () => {
-            if (this.xhr.responseText) {
-                const r = this.xhr.responseText;
-                const list = JSON.parse(r).map(i => ({'label': i.fullname || i.jid, 'value': i.jid}));
-                if (list.length !== 1) {
-                    const el = this.el.querySelector('.invalid-feedback');
-                    el.textContent = __('Sorry, could not find a contact with that name')
-                    u.addClass('d-block', el);
-                    return;
-                }
-                const jid = list[0].value;
-                if (this.validateSubmission(jid)) {
-                    const form = this.el.querySelector('form');
-                    const name = list[0].label;
-                    this.afterSubmission(form, jid, name);
-                }
-            }
-        };
+        return [...new Set(_converse.roster.map(i => i.get('group')).filter(i => i))];
     },
 
     validateSubmission (jid) {
--- src/modals/templates/add-contact.js
+++ src/modals/templates/add-contact.js
@@ -1,8 +1,7 @@
 import { __ } from 'i18n';
-import { api } from '@converse/headless/core.js';
 import { html } from "lit";
 import { modal_header_close_button } from "plugins/modal/templates/buttons.js"
-import { getServerList } from "../../plugins/rosterview/utils"
+import { getServerList } from "../../plugins/rosterview/utils.js"
 
 
 export default (el) => {
@@ -25,16 +24,16 @@ export default (el) => {
                         <span class="modal-alert"></span>
                         <div class="form-group add-xmpp-contact__jid">
                             <label class="clearfix" for="jid">${i18n_xmpp_address}:</label>
-                            <div class="suggestion-box suggestion-box__jid">
-                                <ul class="suggestion-box__results suggestion-box__results--below" hidden=""></ul>
-                                <converse-autocomplete name="jid" .getAutoCompleteList="${getServerList}"
-                                    placeholder="${i18n_contact_placeholder}"/>
-                                <span class="suggestion-box__additions visually-hidden" role="status" aria-live="assertive" aria-relevant="additions"></span>
-                            </div>
+                            <converse-autocomplete
+                                autofocus
+                                .getAutoCompleteList=${getServerList}
+                                placeholder="${i18n_contact_placeholder}"
+                                name="jid"></converse-autocomplete>
                         </div>
 
                         <div class="form-group add-xmpp-contact__name">
                             <label class="clearfix" for="name">${i18n_nickname}:</label>
+                            <!-- TODO: Add converse-autocomplete here as well -->
                             <div class="suggestion-box suggestion-box__name">
                                 <ul class="suggestion-box__results suggestion-box__results--above" hidden=""></ul>
                                 <input type="text" name="name" value="${el.model.get('nickname') || ''}"
@@ -45,7 +44,10 @@ export default (el) => {
 
                         <div class="form-group add-xmpp-contact__group">
                             <label class="clearfix" for="name">${i18n_group}:</label>
-                            <!--<converse-autocomplete .getAutoCompleteList="${() => el.getGroupsAutoCompleteList()}" name="group"/>-->
+                            <converse-autocomplete
+                                .getAutoCompleteList="${() => el.getGroupsAutoCompleteList()}"
+                                name="group">
+                            </converse-autocomplete>
                         </div>
 
                         <div class="form-group"><div class="invalid-feedback">${i18n_error_message}</div></div>
